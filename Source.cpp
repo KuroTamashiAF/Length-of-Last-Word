ca6825e2eaac64f4a8b1c102344ebc4522046e8f
Fedorov <chugun777akk@mail.ru>
2023-04-03 17:32:20 +0700

    all testa passed

--- Source.cpp
+++ Source.cpp
@@ -1,14 +1,35 @@
 #include <iostream>
+#include <vector>
+#include<stack>
 
 using namespace std;
 
 class Solution {
 public:
     static int lengthOfLastWord(string s) {
+        string bb = s;
+        if (bb.empty())
+            return 0;
+       bb.erase(0, bb.find_first_not_of(" "));
+       bb.erase(bb.find_last_not_of(" ")+1);
+             
+       
+        int count = 0;
 
+        for (int i = bb.length()-1; i>=0; i--)
+        {
+           
+            if (bb.at(i) != ' ')
+                count++;
+            else
+            {
+                break;
+            }
 
+           
 
-
+        }
+        return count;
     }
 };
 
@@ -16,7 +37,7 @@ public:
 
 int main()
 {
-    string aaa = "Hello World";
+    string aaa = "   fly me   to   the moon  ";
 
     cout << Solution::lengthOfLastWord(aaa) << endl;
     return 0;
