fd937eac7ef89a260af4193b44d3b6cf821a35e8
Fedorov <chugun777akk@mail.ru>
2023-04-03 15:10:04 +0700

    add source

---
+++ Source.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    static int lengthOfLastWord(string s) {
+
+
+
+
+    }
+};
+
+
+
+
+
+
+
+int main()
+{
+    string aaa = "dlgcdgkv";
+
+    cout << Solution::lengthOfLastWord(aaa) << endl;
+    return 0;
+}
